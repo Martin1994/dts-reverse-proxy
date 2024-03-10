512cefd3327d5e6e29c1dcea3c11cdabcbf2ed87
Martin Chloride <i@martincl2.me>
2024-03-10 13:23:16 -0700

    Use PHP 8.3

--- src/phpConfig.ts
+++ src/phpConfig.ts
@@ -1,5 +1,5 @@
 import { PhpFpmOptions } from "./php-fpm";
 
 export const PHP_CONFIG = {
-    sockFile: "/run/php/php8.1-fpm.sock"
+    sockFile: "/run/php/php8.3-fpm.sock"
 } satisfies PhpFpmOptions;
